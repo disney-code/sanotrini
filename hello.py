05b81384e0182c6f90109dbfa4a696dfff9d6511
Charlene <charlenetansijie@gmail.com>
2023-05-09 18:32:21 +0800

    change divison branch to divisionNewBranch

--- hello.py
+++ hello.py
@@ -5,8 +5,13 @@ def summation(a,b):
 
 def product(a,b):
     return a*b
+
+def divisionNewBranch(a,b):
+    return a/b
 a=int(input("enter a number: "))
 b=int(input("second nnumber: "))
 c=summation(a,b)
 print("the summation of is ", c)
 print(f"the product of {a} * {b} is",product(a,b))
+temp1=divisionNewBranch(a,b)
+print(f"{a}/{b} is ", temp1)
