bde313f43769292fedc975b81634e30542021dc6
Charlene <charlenetansijie@gmail.com>
2023-05-09 18:22:40 +0800

    added division function

--- hello.py
+++ hello.py
@@ -5,8 +5,13 @@ def summation(a,b):
 
 def product(a,b):
     return a*b
+
+def division(a,b):
+    return a/b
 a=int(input("enter a number: "))
 b=int(input("second nnumber: "))
 c=summation(a,b)
 print("the summation of is ", c)
 print(f"the product of {a} * {b} is",product(a,b))
+temp1=division(a,b)
+print(f"{a}/{b} is ", temp1)
